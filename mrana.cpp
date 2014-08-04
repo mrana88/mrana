432abe8682f9468e1022d56a1c7bac226affaf98
RANA <muhammadrana@mail.usf.edu>
2014-08-04 01:08:26 -0400

    more changes

--- mrana.cpp
+++ mrana.cpp
@@ -6,7 +6,7 @@ int main()
 	cout<<"git learner"<<endl;
 	cout<<"making changes in the branch"<<endl;  // more changes 
 	cout<<"i am in master now"<<endl;
-
+	cout<<"merge is loca but pull is remote"<<endl;
 
 
 
